81fbb7ccbf6e0a4137f8c3508fb98d3e1a80657d
xiaoxiaojingge <2427259171@qq.com>
2021-05-17 16:36:24 +0800

    代码整理

---
+++ springsecurity-study/src/test/java/com/itjing/SpringsecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itjing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
