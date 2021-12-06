bda1a7942c96def76e8c136b28c70e286de50756
lijing <2427259171@qq.com>
2021-12-06 10:59:30 +0800

    整理工具类

---
+++ mybatis-generator/src/main/java/com/itjing/pojo/Teacher.java
@@ -0,0 +1,42 @@
+package com.itjing.pojo;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author lijing
+ * @date 2021年12月06日 9:43
+ * @description
+ */
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+@XStreamAlias("Teacher")
+@XmlRootElement(name = "Teacher")
+public class Teacher {
+    @XStreamAlias("id")
+    @XStreamAsAttribute
+    private Integer id;
+
+    @XStreamAlias("name")
+    private String name;
+
+    @XStreamAlias("gender")
+    private String gender;
+
+    @XStreamAlias("age")
+    private Integer age;
+
+    @XStreamAlias("homeAddress")
+    private String home;
+
+    @XStreamAlias("major")
+    private String major;
+}
